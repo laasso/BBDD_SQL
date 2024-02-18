4575def97df584489097c6fd7d31e4ee583c7167
laasso <nrubialesguerreo@gmail.com>
2024-02-18 12:32:14 +0100

    mod 4 server

--- M02UF2_A7_2/agenda-data.sql
+++ M02UF2_A7_2/agenda-data.sql
@@ -1,4 +1,4 @@
-LOAD DATA INFILE '/home/lasso/Escritorio/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
+LOAD DATA INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
 INTO TABLE contacte
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
