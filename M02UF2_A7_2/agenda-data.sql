421c56901f926361444353a9da177223ef972a20
laasso <nrubialesguerreo@gmail.com>
2024-02-18 12:28:18 +0100

    loada data

---
+++ M02UF2_A7_2/agenda-data.sql
@@ -0,0 +1,7 @@
+LOAD DATA INFILE '/home/lasso/Escritorio/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
+INTO TABLE contacte
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(phone, nom, cognom1, cognom2, @date_naixament, correu_electronic)
+SET data_naixament = STR_TO_DATE(@date_naixament, '%Y-%m-%d');
