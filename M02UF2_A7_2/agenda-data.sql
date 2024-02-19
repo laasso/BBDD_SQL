e85755acb3be4da5a4469a3701811aaf996f415e
laasso <nrubialesguerrero@gmail.com>
2024-02-19 08:21:03 +0100

    adaptacio classe

--- M02UF2_A7_2/agenda-data.sql
+++ M02UF2_A7_2/agenda-data.sql
@@ -1,7 +1,7 @@
 USE agenda;
 
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
+LOAD DATA LOCAL INFILE '/home/usuari/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
 INTO TABLE contacte
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
