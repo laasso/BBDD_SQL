8e76392b0475261c73ddae6350896070cf925e1f
laasso <nrubialesguerrero@gmail.com>
2024-02-19 08:44:22 +0100

    erro debugging

--- M02UF3_ImportData/load_csv_xml_json.sql
+++ M02UF3_ImportData/load_csv_xml_json.sql
@@ -1,25 +1,25 @@
-DROP DATABSE IF EXISTS data;
+DROP DATABASE IF EXISTS data;
 CREATE DATABASE IF NOT EXISTS data;
-USE data
+USE data;
 
 CREATE table data_csv (
     id INT auto_increment,
     nom VARCHAR(16),
-    cognom1 VARCHAR(24)
-    cognom2 VARCHAR(24)
+    cognom1 VARCHAR(24),
+    cognom2 VARCHAR(24),
     dataNaixement DATE,
     email VARCHAR(48),
     KEY (id),
     PRIMARY KEY (id)
 );
 
-LOAD DATA INFILE '/home/alumne_1r/BBDD_SQL/M02UF3_ImportData/dades.csv' INTO TABLE data_csv
+LOAD DATA INFILE '/home/usuari/BBDD_SQL/M02UF3_ImportData/dades.csv' INTO TABLE data_csv
 FIELDS TERMINATED BY ',' IGNORE 1 LINES
-(nom, congom1, cognom2, dataNaixement, email) SET id=NULL;
+(nom, cognom1, cognom2, dataNaixement, email) SET id=NULL;
 
 CREATE TABLE data_xml LIKE data_csv;
 
-LOAD XML LOCAL INFILE '/home/alumne_1r/BBDD_SQL/M02UF3_ImportData/dades.csv' INTO TABLE data_xml
+LOAD XML LOCAL INFILE '/home/usuari/BBDD_SQL/M02UF3_ImportData/dades.xml' INTO TABLE data_xml
 ROWS IDENTIFIED BY '<persona>';
 
 CREATE TABLE IF NOT EXISTS data_json(
@@ -27,4 +27,3 @@ CREATE TABLE IF NOT EXISTS data_json(
     dades_json
 );
 
---system mysqlsh root@localhost/data --import dades.json data_json dades --schema=data
