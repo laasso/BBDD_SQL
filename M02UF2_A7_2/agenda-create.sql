098872f9ca72e51bb6c1f9eff7624ba2262cea20
laasso <nrubialesguerrero@gmail.com>
2024-02-20 13:05:07 +0100

    file
    undefined

--- M02UF2_A7_2/agenda-create.sql
+++ M02UF2_A7_2/agenda-create.sql
@@ -51,23 +51,24 @@ CREATE TABLE relacio (
 
 );
 
-
 CREATE TABLE reunions (
-
-	id INT PRIMARY KEY AUTO_INCREMENT,
-
-	contacte1 VARCHAR(15),
-
-	contacte2 VARCHAR(15),
-
-	data_reunio DATETIME,
-
-	durada_hores INT,
-
-	estat ENUM('Pendent', 'Realitzada', 'No realitzada'),
-
-	FOREIGN KEY (contacte1) REFERENCES contacte(mobil),
-
-	FOREIGN KEY (contacte2) REFERENCES contacte(mobil)
-
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    contacte1 VARCHAR(15),
+    contacte2 VARCHAR(15),
+    tipus_reunio ENUM('Presencial', 'Telemàtica'),
+    estat INT,
+    data_reunio DATETIME,
+    hores_reunio INT,
+    descripcio VARCHAR(20) GENERATED ALWAYS AS (
+        CASE 
+            WHEN estat = 1 THEN 'Realitzat'
+            WHEN estat = 0 THEN 'No realitzat'
+            ELSE 'Pendent'
+        END
+    ) STORED,
+    data_registre TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (contacte1) REFERENCES relacio(contacte1),
+    FOREIGN KEY (contacte2) REFERENCES relacio(contacte2)
 );
+
+
