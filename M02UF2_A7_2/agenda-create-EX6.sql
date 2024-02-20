e3ce135e8aa86ac02aef60b4903078ba674d4423
laasso <nrubialesguerrero@gmail.com>
2024-02-20 09:35:17 +0100

    workin sin pendents

--- M02UF2_A7_2/agenda-create-EX6.sql
+++ M02UF2_A7_2/agenda-create-EX6.sql
@@ -49,15 +49,14 @@ CREATE TABLE relacio (
 
 );
 
-
 CREATE TABLE reunions (
     id INT PRIMARY KEY AUTO_INCREMENT,
     contacte1 VARCHAR(15),
     contacte2 VARCHAR(15),
-    data_reunio DATETIME,
-    durada_hores INT,
     tipus_reunio ENUM('Presencial', 'Telemàtica'),
     estat INT,
+    data_reunio DATETIME,
+    hores_reunio INT,
     descripcio VARCHAR(50) GENERATED ALWAYS AS (
         CASE 
             WHEN estat = 1 THEN 'Realitzat'
@@ -68,43 +67,5 @@ CREATE TABLE reunions (
     data_registre TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DELIMITER //
-CREATE PROCEDURE generar_inserts_reunions()
-BEGIN
-    DECLARE i INT DEFAULT 1;
-    DECLARE contacto1 VARCHAR(15);
-    DECLARE contacto2 VARCHAR(15);
-    DECLARE fecha_reunion DATETIME;
-    DECLARE duracion INT;
-    DECLARE tipo_reunion ENUM('Presencial', 'Telemàtica');
-    DECLARE estado INT;
 
-    WHILE i <= 100 DO
-        -- Obtener contactos aleatorios
-        SELECT mobil INTO contacto1 FROM contacte ORDER BY RAND() LIMIT 1;
-        SELECT mobil INTO contacto2 FROM contacte ORDER BY RAND() LIMIT 1;
-        
-        -- Generar fecha aleatoria entre 2 meses antes y después de la fecha y hora actual con un intervalo de 8 horas
-        SET fecha_reunion = NOW() - INTERVAL FLOOR(RAND() * 60) DAY + INTERVAL FLOOR(RAND() * 16 - 8) HOUR;
 
-        -- Generar duracion aleatoria entre 1 y 8 horas
-        SET duracion = FLOOR(RAND() * 8) + 1;
-
-        -- Generar tipo de reunion aleatoria
-        SET tipo_reunion = IF(RAND() > 0.5, 'Presencial', 'Telemàtica');
-
-        -- Generar estado aleatorio
-        IF fecha_reunion < NOW() THEN
-            SET estado = IF(RAND() > 0.5, 1, 0);
-        ELSE
-            SET estado = 0;
-        END IF;
-
-        -- Insertar datos generados en la tabla reunions
-        INSERT INTO reunions (contacte1, contacte2, data_reunio, durada_hores, tipus_reunio, estat)
-        VALUES (contacto1, contacto2, fecha_reunion, duracion, tipo_reunion, estado);
-        
-        SET i = i + 1;
-    END WHILE;
-END //
-DELIMITER ;
--- M02UF2_A7_2/agenda-data.sql
+++ M02UF2_A7_2/agenda-data.sql
@@ -1,5 +1,6 @@
 USE agenda;
 
+/*INSERT CSV SEPARAT PER TABULACIONS*/
 
 LOAD DATA LOCAL INFILE '/home/usuari/BBDD_SQL/M02UF2_A7_2/agenda.contacte.csv.tsv'
 INTO TABLE contacte
@@ -8,3 +9,27 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (mobil, nom, cognom1, cognom2, @data_naixement, correu_electronic)
 SET data_naixement = STR_TO_DATE(@data_naixement, '%Y-%m-%d');
+
+
+/*INSERT REUNIONS ALEATORIES*/
+
+INSERT INTO reunions (contacte1, contacte2, tipus_reunio, estat, data_reunio, hores_reunio)
+SELECT
+    c1.mobil AS contacte1,
+    c2.mobil AS contacte2,
+    IF(RAND() < 0.5, 'Presencial', 'Telemàtica') AS tipus_reunio,
+    CASE
+        WHEN DATE_ADD(NOW(), INTERVAL FLOOR(RAND() * 2 - 1) MONTH) + INTERVAL FLOOR(RAND() * 16 - 8) HOUR < NOW() THEN
+            IF(RAND() < 0.5, 1, 0)
+        ELSE
+            0
+    END AS estat,
+    DATE_ADD(NOW(), INTERVAL FLOOR(RAND() * 2 - 1) MONTH) + INTERVAL FLOOR(RAND() * 16 - 8) HOUR AS data_reunio,
+    FLOOR(RAND() * 8) + 1 AS hores_reunio
+FROM
+    contacte c1
+    CROSS JOIN contacte c2
+ORDER BY RAND()
+LIMIT 100;
+
+
