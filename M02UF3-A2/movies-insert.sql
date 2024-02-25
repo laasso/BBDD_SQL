a347643a83c9c286bc270d1d36cf1a46f14ce523
laasso <nrubialguerrero@gmail.com>
2024-02-25 13:10:20 +0100

    server ok

--- M02UF3-A2/movies-insert.sql
+++ M02UF3-A2/movies-insert.sql
@@ -22,7 +22,7 @@ FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(@dummy, @dummy, @dummy, @dummy, @dummy @dummy, nom_director);
+(@dummy, @dummy, @dummy, @dummy, @dummy, @dummy, nom_director) SET id_director = NULL;
 
 -- Insertar datos en la tabla 'actors' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE actors
@@ -30,7 +30,7 @@ FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_actor, @dummy);
+(@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,nom_actor);
 
 -- Insertar datos en la tabla 'pelicules' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE pelicules
@@ -38,4 +38,4 @@ FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id_pelicula, titol, any, vots,@dummy, @dummy, estudis);
+(id_pelicula, titol, any, vots, @dummy, @dummy, @dummy, estudis);
