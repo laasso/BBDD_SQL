a67eeb66608497481f245a6a4f65aef8339d7e5f
laasso <nrubialesguerreo@gmail.com>
2024-02-25 12:59:22 +0100

    @dummy

--- M02UF3-A2/movies-insert.sql
+++ M02UF3-A2/movies-insert.sql
@@ -1,37 +1,41 @@
 USE movies;
 
+-- Insertar datos en la tabla 'genere' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE genere
-FIELDS TERMINATED BY '\t' 
+FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_genere);
+(@dummy, @dummy, @dummy, @dummy, nom_genere);
 
+-- Insertar datos en la tabla 'pais' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE pais
-FIELDS TERMINATED BY '\t' 
+FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_pais);
+(@dummy, @dummy, @dummy, @dummy, @dummy, nom_pais);
 
+-- Insertar datos en la tabla 'directors' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE directors
-FIELDS TERMINATED BY '\t' 
+FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_director);
+(@dummy, @dummy, @dummy, @dummy, @dummy @dummy, nom_director);
 
+-- Insertar datos en la tabla 'actors' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE actors
-FIELDS TERMINATED BY '\t' 
+FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom_actor);
+(nom_actor, @dummy);
 
+-- Insertar datos en la tabla 'pelicules' desde el archivo CSV
 LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE pelicules
-FIELDS TERMINATED BY '\t' 
+FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(@id_movie, titol, any, vots, @genere, @pais, @director, estudis, @actor, @rol)
-SET id_pelicula = @id_movie;
+(id_pelicula, titol, any, vots,@dummy, @dummy, estudis);
