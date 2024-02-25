f931250fc41454245d0c9a6414d38bfe4ccbffb4
laasso <nrubialesguerreo@gmail.com>
2024-02-25 12:40:18 +0100

    path correction

--- M02UF3-A2/movies-insert.sql
+++ M02UF3-A2/movies-insert.sql
@@ -1,34 +1,34 @@
 USE movies;
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE genere
+LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE genere
 FIELDS TERMINATED BY '\t' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_genere);
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE pais
+LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE pais
 FIELDS TERMINATED BY '\t' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_pais);
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE directors
+LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE directors
 FIELDS TERMINATED BY '\t' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_director);
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE actors
+LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE actors
 FIELDS TERMINATED BY '\t' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (nom_actor);
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE pelicules
+LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/raspi_json_movies.csv' INTO TABLE pelicules
 FIELDS TERMINATED BY '\t' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
