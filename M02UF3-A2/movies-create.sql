085899c5d23fbb4140911fd832da466df7495c8f
laasso <nrubialesguerreo@gmail.com>
2024-02-25 12:39:12 +0100

    name change

--- M02UF3-A2/movies-create.sql
+++ M02UF3-A2/movies-create.sql
@@ -1,36 +1,66 @@
-USE movies;
+    DROP DATABASE IF EXISTS movies;
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE genere
-FIELDS TERMINATED BY '\t' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(nom_genere);
+    CREATE DATABASE movies;
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE pais
-FIELDS TERMINATED BY '\t' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(nom_pais);
+    USE movies;
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE directors
-FIELDS TERMINATED BY '\t' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(nom_director);
+    CREATE TABLE pelicules (
+        id_pelicula INT AUTO_INCREMENT PRIMARY KEY,
+        titol VARCHAR(255) NOT NULL,
+        any INT,
+        vots INT,
+        estudis VARCHAR(255)
+    );
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE actors
-FIELDS TERMINATED BY '\t' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(nom_actor);
+    CREATE TABLE genere (
+        id_genere INT AUTO_INCREMENT PRIMARY KEY,
+        nom_genere VARCHAR(100) UNIQUE
+    );
 
-LOAD DATA LOCAL INFILE '/home/usuari/Escritorio/BBDD_SQL/M02UF3-A2/movie-create.sql' INTO TABLE pelicules
-FIELDS TERMINATED BY '\t' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(titol, any, vots, estudis);
+    CREATE TABLE pais (
+        id_pais INT AUTO_INCREMENT PRIMARY KEY,
+        nom_pais VARCHAR(255) UNIQUE
+    );
+
+    CREATE TABLE directors (
+        id_director INT AUTO_INCREMENT PRIMARY KEY,
+        nom_director VARCHAR(255) UNIQUE
+    );
+
+    CREATE TABLE actors (
+        id_actor INT AUTO_INCREMENT PRIMARY KEY,
+        nom_actor VARCHAR(255) UNIQUE
+    );
+
+    CREATE TABLE pelicules_genere (
+        id_pelicula INT,
+        id_genere INT,
+        PRIMARY KEY (id_pelicula, id_genere),
+        FOREIGN KEY (id_pelicula) REFERENCES pelicules(id_pelicula),
+        FOREIGN KEY (id_genere) REFERENCES genere(id_genere)
+    );
+
+    CREATE TABLE pelicules_pais (
+        id_pelicula INT,
+        id_pais INT,
+        PRIMARY KEY (id_pelicula, id_pais),
+        FOREIGN KEY (id_pelicula) REFERENCES pelicules(id_pelicula),
+        FOREIGN KEY (id_pais) REFERENCES pais(id_pais)
+    );
+
+    CREATE TABLE pelicules_directors (
+        id_pelicula INT,
+        id_director INT,
+        PRIMARY KEY (id_pelicula, id_director),
+        FOREIGN KEY (id_pelicula) REFERENCES pelicules(id_pelicula),
+        FOREIGN KEY (id_director) REFERENCES directors(id_director)
+    );
+
+    CREATE TABLE pelicules_actors (
+        id_pelicula INT,
+        id_actor INT,
+        rol VARCHAR(255),
+        PRIMARY KEY (id_pelicula, id_actor, rol),
+        FOREIGN KEY (id_pelicula) REFERENCES pelicules(id_pelicula),
+        FOREIGN KEY (id_actor) REFERENCES actors(id_actor)
+    );
