9dcc141343da991c1a887e4880a4eb15f4956ba7
laasso <nrubialesguerreo@gmail.com>
2024-03-10 12:18:13 +0100

    isnert

--- M02UF2-A8/roba_insert.sql
+++ M02UF2-A8/roba_insert.sql
@@ -15,26 +15,10 @@ INSERT INTO CLASE (id_clase, id_categoria, nom_clase, descripcio) VALUES
 (7, 3, 'Roba de dona', 'Vestits, faldilles, pantalons, etc.');
 
 -- Inserts para la tabla TALLA
--- Inserts para la tabla TALLA
 INSERT INTO TALLA (id_talla, nom) VALUES
-(4, '0 mesos'),        
-(5, '1 mes'),          
-(6, '3 mesos'),        
-(7, '6 mesos'),        
-(8, '12 mesos'),       
-(9, '24 mesos'),       
-(10, '4'),             
-(11, '6'),             
-(12, '8'),             
-(13, '10'),            
-(14, '12'),            
-(15, '14'),            
-(16, 'S (petita)'),   
-(17, 'M (mitjana)'),  
-(18, 'L (gran)'),     
-(19, 'XL (super gran)'), 
-(20, 'XXL (extra gran)'); 
-
+(1, 'XS'), (2, 'S'), (3, 'M'), (4, 'L'), (5, 'XL'),
+(6, 'XXL'), (7, '0-3 mesos'), (8, '3-6 mesos'), (9, '6-12 mesos'), (10, '12-18 mesos'),
+(11, '4'), (12, '6'), (13, '8'), (14, '10'), (15, '12'), (16, '14');
 
 -- Inserts para la tabla COLOR
 INSERT INTO COLOR (id_color, nom) VALUES
@@ -52,15 +36,15 @@ INSERT INTO MODELO (id_modelo, nom) VALUES
 (6, 'Floral'), (7, 'A ratlles'), (8, 'A quadres'), (9, 'Degradat'), (10, 'Llis');
 
 -- Inserts para la tabla ARTICLE
--- Inserts para la tabla ARTICLE
+-- Inserts adicionales para la tabla ARTICLE
 INSERT INTO ARTICLE (id_article, nom, id_categoria, id_clase, id_talla, id_color, id_material, id_modelo, stock, preu_cost, preu_venda) VALUES
-(1, 'Joc de llençols', 1, 1, NULL, 1, 1, 1, 50, 15.99, 29.99),
-(2, 'Tovalloles de bany', 1, 2, NULL, 3, 1, 1, 100, 8.99, 19.99),
-(3, 'Cinturó de cuir', 2, 3, NULL, NULL, 9, 1, 30, 12.5, 29.99),
-(4, 'Samarreta de nadó', 3, 4, 7, 8, 1, 5, 80, 7.99, 15.99),
-(5, 'Vestit de nena', 3, 5, 8, 8, 1, 3, 60, 14.99, 29.99),
-(6, 'Pantalons dhome', 3, 6, 2, 1, 6, 1, 120, 22.5, 49.99),
-(7, 'Vestit de dona', 3, 7, 4, 2, 1, 3, 70, 29.99, 59.99),
-(8, 'Bufanda de llana', 2, 3, NULL, NULL, 8, 8, 40, 9.99, 19.99),
-(9, 'Jeans', 3, 6, 2, 1, 10, 4, 90, 18.99, 39.99),
-(10, 'Vestit casual', 3, 7, 4, 5, 1, 4, 50, 39.99, 79.99);
+(11, 'Camiseta de algodón', 3, 5, 2, 1, 1, 1, 60, 8.99, 19.99),
+(12, 'Pantalones vaqueros', 3, 6, 13, 2, 10, 4, 40, 24.99, 49.99),
+(13, 'Vestido de fiesta', 3, 7, 14, 8, 4, 3, 30, 49.99, 99.99),
+(14, 'Bufanda de lana', 2, 3, NULL, NULL, 8, 8, 80, 12.99, 24.99),
+(15, 'Zapatos deportivos', 3, 2, NULL, 5, 6, 2, 50, 29.99, 59.99),
+(16, 'Chaqueta de cuero', 3, 6, 12, 4, 9, 1, 20, 79.99, 149.99),
+(17, 'Pijama de niña', 3, 4, 10, 1, 1, 5, 35, 15.99, 29.99),
+(18, 'Chaqueta elegante', 3, 7, 14, 2, 9, 3, 25, 59.99, 119.99),
+(19, 'Sudadera con capucha', 3, 6, 12, 6, 5, 4, 45, 19.99, 39.99),
+(20, 'Falda plisada', 3, 7, 12, 8, 1, 3, 30, 29.99, 59.99);
