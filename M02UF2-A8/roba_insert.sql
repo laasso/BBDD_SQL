f2c6d41fd0237d5d9858d52138be0b86e2caf22c
laasso <nrubialesguerreo@gmail.com>
2024-03-10 12:19:37 +0100

    UPDATE

--- M02UF2-A8/roba_insert.sql
+++ M02UF2-A8/roba_insert.sql
@@ -38,13 +38,20 @@ INSERT INTO MODELO (id_modelo, nom) VALUES
 -- Inserts para la tabla ARTICLE
 -- Inserts adicionales para la tabla ARTICLE
 INSERT INTO ARTICLE (id_article, nom, id_categoria, id_clase, id_talla, id_color, id_material, id_modelo, stock, preu_cost, preu_venda) VALUES
-(11, 'Camiseta de algodón', 3, 5, 2, 1, 1, 1, 60, 8.99, 19.99),
-(12, 'Pantalones vaqueros', 3, 6, 13, 2, 10, 4, 40, 24.99, 49.99),
-(13, 'Vestido de fiesta', 3, 7, 14, 8, 4, 3, 30, 49.99, 99.99),
-(14, 'Bufanda de lana', 2, 3, NULL, NULL, 8, 8, 80, 12.99, 24.99),
-(15, 'Zapatos deportivos', 3, 2, NULL, 5, 6, 2, 50, 29.99, 59.99),
-(16, 'Chaqueta de cuero', 3, 6, 12, 4, 9, 1, 20, 79.99, 149.99),
-(17, 'Pijama de niña', 3, 4, 10, 1, 1, 5, 35, 15.99, 29.99),
-(18, 'Chaqueta elegante', 3, 7, 14, 2, 9, 3, 25, 59.99, 119.99),
-(19, 'Sudadera con capucha', 3, 6, 12, 6, 5, 4, 45, 19.99, 39.99),
-(20, 'Falda plisada', 3, 7, 12, 8, 1, 3, 30, 29.99, 59.99);
+(1, 'Joc de llençols', 1, 1, NULL, 1, 1, 1, 50, 15.99, 29.99),
+(2, 'Tovalloles de bany', 1, 2, NULL, 3, 1, 1, 100, 8.99, 19.99),
+(3, 'Cinturó de cuir', 2, 3, NULL, NULL, 9, 1, 30, 12.5, 29.99),
+(4, 'Samarreta de nadó', 3, 4, 7, 8, 1, 5, 80, 7.99, 15.99),
+(5, 'Vestit de nena', 3, 5, 8, 8, 1, 3, 60, 14.99, 29.99),
+(6, 'Pantalons dhome', 3, 6, 2, 1, 6, 1, 120, 22.5, 49.99),
+(7, 'Vestit de dona', 3, 7, 4, 2, 1, 3, 70, 29.99, 59.99);
+(8, 'Camiseta de algodón', 3, 5, 2, 1, 1, 1, 60, 8.99, 19.99),
+(9, 'Pantalones vaqueros', 3, 6, 13, 2, 10, 4, 40, 24.99, 49.99),
+(10, 'Vestido de fiesta', 3, 7, 14, 8, 4, 3, 30, 49.99, 99.99),
+(11, 'Bufanda de lana', 2, 3, NULL, NULL, 8, 8, 80, 12.99, 24.99),
+(12, 'Zapatos deportivos', 3, 2, NULL, 5, 6, 2, 50, 29.99, 59.99),
+(13, 'Chaqueta de cuero', 3, 6, 12, 4, 9, 1, 20, 79.99, 149.99),
+(14, 'Pijama de niña', 3, 4, 10, 1, 1, 5, 35, 15.99, 29.99),
+(15, 'Chaqueta elegante', 3, 7, 14, 2, 9, 3, 25, 59.99, 119.99),
+(16, 'Sudadera con capucha', 3, 6, 12, 6, 5, 4, 45, 19.99, 39.99),
+(17, 'Falda plisada', 3, 7, 12, 8, 1, 3, 30, 29.99, 59.99);
