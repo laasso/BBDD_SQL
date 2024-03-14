0d0161cdf2dde1a5bc9113e65d856fda164b6533
laasso <nrubialesguerrero@gmail.com>
2024-03-14 12:12:52 +0100

    reverse engineer

--- M02UF2-A8/roba_create.sql
+++ M02UF2-A8/roba_create.sql
@@ -4,49 +4,48 @@ CREATE DATABASE roba;
 
 USE roba;
 
--- Creación de la tabla CATEGORIA
-CREATE TABLE CATEGORIA (
+-- Creación de la tabla categoria
+CREATE TABLE categoria (
     id_categoria INT PRIMARY KEY,
     nom_cat VARCHAR(255),
     descripcio VARCHAR(255)
 );
 
--- Creación de la tabla CLASE
-CREATE TABLE CLASE (
+-- Creación de la tabla clase
+CREATE TABLE clase (
     id_clase INT PRIMARY KEY,
     id_categoria INT,
     nom_clase VARCHAR(255),
     descripcio VARCHAR(255),
-    FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id_categoria)
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
 
--- Creación de la tabla COLOR
-CREATE TABLE COLOR (
+-- Creación de la tabla color
+CREATE TABLE color (
     id_color INT PRIMARY KEY,
     nom VARCHAR(50)
 );
 
--- Creación de la tabla MATERIAL
-CREATE TABLE MATERIAL (
+-- Creación de la tabla material
+CREATE TABLE material (
     id_material INT PRIMARY KEY,
     nom VARCHAR(50)
 );
 
--- Creación de la tabla MODELO
-CREATE TABLE MODELO (
+-- Creación de la tabla modelo
+CREATE TABLE modelo (
     id_modelo INT PRIMARY KEY,
     nom VARCHAR(50)
 );
 
--- Creación de la tabla TIPUS
-CREATE TABLE TIPUS (
+-- Creación de la tabla tipus
+CREATE TABLE tipus (
     id_tipus INT PRIMARY KEY,
     nom VARCHAR(50)
 );
 
-
--- Creación de la tabla ARTICLE
-CREATE TABLE ARTICLE (
+-- Creación de la tabla article
+CREATE TABLE article (
     id_article INT PRIMARY KEY,
     nom VARCHAR(255),
     id_categoria INT,
@@ -55,53 +54,56 @@ CREATE TABLE ARTICLE (
     id_color INT,
     id_material INT,
     id_modelo INT,
-    stock INT,
     preu_cost FLOAT,
     preu_venda FLOAT,
-    FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id_categoria),
-    FOREIGN KEY (id_clase) REFERENCES CLASE(id_clase),
-    FOREIGN KEY (id_color) REFERENCES COLOR(id_color),
-    FOREIGN KEY (id_material) REFERENCES MATERIAL(id_material),
-    FOREIGN KEY (id_modelo) REFERENCES MODELO(id_modelo),
-    FOREIGN KEY (id_tipus) REFERENCES TIPUS(id_tipus)
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+    FOREIGN KEY (id_clase) REFERENCES clase(id_clase),
+    FOREIGN KEY (id_color) REFERENCES color(id_color),
+    FOREIGN KEY (id_material) REFERENCES material(id_material),
+    FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo),
+    FOREIGN KEY (id_tipus) REFERENCES tipus(id_tipus)
 );
 
--- Creación de la tabla MIDA
-CREATE TABLE MIDA (
+-- Creación de la tabla mida
+CREATE TABLE mida (
     id_article INT,
     amplada INT,
     llargada INT,
-    FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
+    FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
 
--- Creación de la tabla TALLA_ADULTS_SUPERIOR
-CREATE TABLE TALLA_ADULTS_SUPERIOR (
+-- Creación de la tabla talla_adults_superior
+CREATE TABLE talla_adults_superior (
     id_article INT,
     nom_talla VARCHAR(50),
+    stock INT,
     PRIMARY KEY (id_article, nom_talla),
-    FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
+    FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
 
--- Creación de la tabla TALLA_ADULTS_INFERIOR
-CREATE TABLE TALLA_ADULTS_INFERIOR (
+-- Creación de la tabla talla_adults_inferior
+CREATE TABLE talla_adults_inferior (
     id_article INT,
     nom_talla VARCHAR(50),
+    stock INT,
     PRIMARY KEY (id_article, nom_talla),
-    FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
+    FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
 
--- Creación de la tabla TALLA_NADONS
-CREATE TABLE TALLA_NADONS (
+-- Creación de la tabla talla_nadons
+CREATE TABLE talla_nadons (
     id_article INT,
     nom_talla VARCHAR(50),
+    stock INT,
     PRIMARY KEY (id_article, nom_talla),
-    FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
+    FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
 
--- Creación de la tabla TALLA_NENS
-CREATE TABLE TALLA_NENS (
+-- Creación de la tabla talla_nens
+CREATE TABLE talla_nens (
     id_article INT,
     nom_talla VARCHAR(50),
+    stock INT,
     PRIMARY KEY (id_article, nom_talla),
-    FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
+    FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
--- M02UF2-A8/roba_insert.sql
+++ M02UF2-A8/roba_insert.sql
@@ -1,11 +1,11 @@
--- Inserts para la tabla CATEGORIA
-INSERT INTO CATEGORIA (id_categoria, nom_cat, descripcio) VALUES
+-- Inserts para la tabla categoria
+INSERT INTO categoria (id_categoria, nom_cat, descripcio) VALUES
 (1, 'Roba de casa', 'Llençols, estovalles, tovallons, etc.'),
 (2, 'Complements', 'Cinturons, gorres, bufandes, etc.'),
 (3, 'Roba de vestir', 'Ropa de nadons, nens, home i dona');
 
--- Inserts para la tabla CLASE
-INSERT INTO CLASE (id_clase, id_categoria, nom_clase, descripcio) VALUES
+-- Inserts para la tabla clase
+INSERT INTO clase (id_clase, id_categoria, nom_clase, descripcio) VALUES
 (1, 1, 'Roba de llit', 'Llençols, estovalles, fundes, etc.'),
 (2, 1, 'Roba de bany', 'Tovalloles de bany, estores, etc.'),
 (3, 2, 'Accessoris', 'Cinturons, bufandes, gorres, mocadors, etc.'),
@@ -14,41 +14,68 @@ INSERT INTO CLASE (id_clase, id_categoria, nom_clase, descripcio) VALUES
 (6, 3, 'Roba home', 'Camises, pantalons, americanes, etc.'),
 (7, 3, 'Roba de dona', 'Vestits, faldilles, pantalons, etc.');
 
--- Inserts para la tabla TALLA
-INSERT INTO TALLA (id_talla, nom) VALUES
-(1, 'XS'), (2, 'S'), (3, 'M'), (4, 'L'), (5, 'XL'),
-(6, 'XXL'), (7, '0-3 mesos'), (8, '3-6 mesos'), (9, '6-12 mesos'), (10, '12-18 mesos'),
-(11, '4'), (12, '6'), (13, '8'), (14, '10'), (15, '12'), (16, '14');
+-- Inserts para la tabla talla_nadons
+INSERT INTO talla_nadons (id_article, nom_talla) VALUES
+(6, '0-3 mesos'), 
+(7, '3-6 mesos'), 
+(8, '6-12 mesos'), 
+(9, '12-18 mesos'), 
+(10, '18-24 mesos');
 
--- Inserts para la tabla COLOR
-INSERT INTO COLOR (id_color, nom) VALUES
+-- Inserts para la tabla talla_nens
+INSERT INTO talla_nens (id_article, nom_talla) VALUES
+(5, '4'), 
+(6, '6'), 
+(7, '8'), 
+(8, '10'), 
+(9, '12'), 
+(10, '14');
+
+-- Inserts para la tabla talla_adults_superior
+INSERT INTO talla_adults_superior (id_article, nom_talla) VALUES
+(11, 'S'), 
+(12, 'M'), 
+(13, 'L'), 
+(14, 'XL'), 
+(15, 'XXL');
+
+-- Inserts para la tabla talla_adults_inferior
+INSERT INTO talla_adults_inferior (id_article, nom_talla) VALUES
+(1, '34'), 
+(2, '36'), 
+(3, '38'), 
+(4, '40'), 
+(5, '42');
+
+-- Inserts para la tabla color
+INSERT INTO color (id_color, nom) VALUES
 (1, 'Blanc'), (2, 'Negre'), (3, 'Blau'), (4, 'Gris'), (5, 'Vermell'),
 (6, 'Verd'), (7, 'Groc'), (8, 'Rosa'), (9, 'Marró'), (10, 'Taronja');
 
--- Inserts para la tabla MATERIAL
-INSERT INTO MATERIAL (id_material, nom) VALUES
+-- Inserts para la tabla material
+INSERT INTO material (id_material, nom) VALUES
 (1, 'Cotó'), (2, 'Lli'), (3, 'Tergal'), (4, 'Seda'), (5, 'Felpa'),
 (6, 'Polièster'), (7, 'Nylon'), (8, 'Llana'), (9, 'Pell'), (10, 'Denim');
 
--- Inserts para la tabla MODELO
-INSERT INTO MODELO (id_modelo, nom) VALUES
+-- Inserts para la tabla modelo
+INSERT INTO modelo (id_modelo, nom) VALUES
 (1, 'Levis501'), (2, 'NikeFit'), (3, 'Elegant'), (4, 'Casual'), (5, 'Estampat'),
 (6, 'Floral'), (7, 'A ratlles'), (8, 'A quadres'), (9, 'Degradat'), (10, 'Llis');
 
-INSERT INTO TIPUS (id_tipus, nom) VALUES
-(1, 'Pantalo Llarg'), (2, 'Samarreta'), (3, 'Pantalo Curt'), (4, 'Casual'), (5, 'Estampat'),
-(6, 'Floral'), (7, 'A ratlles'), (8, 'A quadres'), (9, 'Degradat'), (10, 'Llis');
+-- Inserts para la tabla tipus
+INSERT INTO tipus (id_tipus, nom) VALUES
+(1, 'Pantalo Llarg'), (2, 'Samarreta'), (3, 'Pantalo Curt'), (4, 'Camisa'), (5, 'Pantalo Esportiu'),
+(6, 'Vestit'), (7, 'Pantalo Regular Fit'), (8, 'A quadres'), (9, 'Degradat'), (10, 'Llis');
 
 
--- Inserts para la tabla ARTICLE
--- Inserts adicionales para la tabla ARTICLE
-INSERT INTO ARTICLE (id_article, nom, id_categoria, id_clase, id_talla, id_color, id_material, id_modelo, stock, preu_cost, preu_venda) VALUES
-(1, 'Pantalo Texa Basic', 3, 6, 3, 3, 10, 1, 50, 15.99, 29.99),
-(2, 'Pantalo Texa Basic', 3, 6, 4, 3, 10, 1, 50, 15.99, 29.99),
-(3, 'Pantalo Texa Basic', 3, 6, 5, 3, 10, 1, 50, 15.99, 29.99),
-(4, 'Pantalo Texa Basic', 3, 6, 6, 3, 10, 1, 50, 15.99, 29.99),
-(5, 'Pantalo Texa Basic', 3, 5, 6, 3, 10, 1, 50, 15.99, 29.99),
-(6, 'Vestit de nena', 3, 5, 8, 8, 1, 3, 60, 14.99, 29.99),
+-- Inserts para la tabla article
+INSERT INTO article (id_article, nom, id_categoria, id_clase, id_color, id_material, id_modelo, stock, preu_cost, preu_venda) VALUES
+(1, 'Pantalo Texa Basic', 3, 6, 3, 10, 1, 50, 15.99, 29.99),
+(2, 'Pantalo Texa Basic', 3, 6, 3, 10, 1, 50, 15.99, 29.99),
+(3, 'Pantalo Texa Basic', 3, 6, 3, 10, 1, 50, 15.99, 29.99),
+(4, 'Pantalo Texa Basic', 3, 6, 3, 10, 1, 50, 15.99, 29.99),
+(5, 'Pantalo Texa Basic', 3, 5, 3, 10, 1, 50, 15.99, 29.99),
+(6, 'Vestit de nena', 3, 4, 8, 1, 3, 60, 14.99, 29.99),
 (7, 'Vestit de dona', 3, 7, 4, 2, 1, 3, 70, 29.99, 59.99),
 (8, 'Samareta de cotó', 3, 5, 2, 1, 1, 1, 60, 8.99, 19.99),
 (9, 'Pantalons de texans', 3, 6, 13, 2, 10, 4, 40, 24.99, 49.99),
@@ -60,4 +87,3 @@ INSERT INTO ARTICLE (id_article, nom, id_categoria, id_clase, id_talla, id_color
 (15, 'Jaqueta elegant', 3, 7, 14, 2, 9, 3, 25, 59.99, 119.99),
 (16, 'Sudadera amb caputxa', 3, 6, 12, 6, 5, 4, 45, 19.99, 39.99),
 (17, 'Faldilla plisada', 3, 7, 12, 8, 1, 3, 30, 29.99, 59.99);
-
