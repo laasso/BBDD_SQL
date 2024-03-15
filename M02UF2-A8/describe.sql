3b272ab8bf91b280beff5397b66a8cc5451c4563
laasso <nrubialesguerrero@gmail.com>
2024-03-15 13:37:47 +0100

    desc

---
+++ M02UF2-A8/describe.sql
@@ -0,0 +1,25 @@
+-- DESCRIBE de cada tabla
+DESCRIBE categoria;
+DESCRIBE clase;
+DESCRIBE color;
+DESCRIBE material;
+DESCRIBE modelo;
+DESCRIBE tipus;
+DESCRIBE article;
+DESCRIBE mida;
+DESCRIBE talla_adults_superior;
+DESCRIBE talla_nadons;
+DESCRIBE talla_nens;
+
+-- Select * FROM nomtaula limit 3; para cada tabla
+SELECT * FROM categoria LIMIT 3;
+SELECT * FROM clase LIMIT 3;
+SELECT * FROM color LIMIT 3;
+SELECT * FROM material LIMIT 3;
+SELECT * FROM modelo LIMIT 3;
+SELECT * FROM tipus LIMIT 3;
+SELECT * FROM article LIMIT 3;
+SELECT * FROM mida LIMIT 3;
+SELECT * FROM talla_adults_superior LIMIT 3;
+SELECT * FROM talla_nadons LIMIT 3;
+SELECT * FROM talla_nens LIMIT 3;
