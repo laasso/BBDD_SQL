9e1e006a02310fa7498bf8ac6422a9edb30aa5ce
laasso <nrubialesguerrero@gmail.com>
2024-04-17 10:15:13 +0200

    main changes

--- M02UF2-A9/QUERYS/unitaula.sql
+++ M02UF2-A9/QUERYS/unitaula.sql
@@ -1,3 +1,13 @@
 -- Mostar el preu mitja per comprar un pis de mes de 2 dormitoris i any de cionstruccio superior a 2005
 SELECT AVG(preu_venda) AS Preu_Mitja FROM vivenda
 WHERE dormitoris > 2 AND any_construccio > 2005;
+
+-- Mostrar el codi INE amb el nom dels municipis que tinguin id comarca 2
+SELECT codi_INE AS INE , nom_municipi AS Nom FROM municipi
+WHERE id_comarca = 2;
+
+-- Mostrar els pisos duplex agrupats per numero de planta
+
+SELECT * FROM pis
+WHERE tipus = 'Dúplex'
+GROUP BY num_planta;
---
+++ M02UF2-A9/SQL/create.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS habitatge;
+CREATE DATABASE IF NOT EXISTS habitatge;
+USE habitatge;
+
+CREATE TABLE `vivenda` (
+    id_vivenda INT AUTO_INCREMENT PRIMARY KEY,
+    tipus VARCHAR(24),
+    dormitoris VARCHAR(24),
+    banys INT,
+    superficie_vivenda DECIMAL(8, 2),
+    superficie_terrassa DECIMAL(8, 2),
+    any_construccio INT,
+    estat_conservacio ENUM('segunda mano/buen estado', 'segunda mano/para reformar', 'promoción de obra nueva', ''),
+    preu_venda DECIMAL(10, 2),
+    disponible_lloguer BOOLEAN,
+    descripcio VARCHAR(140),
+    data DATE
+);
+
+CREATE TABLE `casa` (
+    id_vivenda INT AUTO_INCREMENT PRIMARY KEY,
+    tipus VARCHAR(24),
+    num_plantes VARCHAR(48),
+    superficie_garatge DECIMAL(8, 2),
+    superficie_jardi DECIMAL(8, 2),
+    FOREIGN KEY (id_vivenda) REFERENCES vivenda(id_vivenda)
+);
+
+CREATE TABLE `pis` (
+    id_vivenda INT AUTO_INCREMENT PRIMARY KEY,
+    tipus VARCHAR(24),
+    num_planta VARCHAR(48),
+    FOREIGN KEY (id_vivenda) REFERENCES vivenda(id_vivenda)
+);
+
+CREATE TABLE `caracteristiques` (
+    id_caracteristica INT AUTO_INCREMENT PRIMARY KEY,
+    aire_condicionat BOOLEAN,
+    ascensor BOOLEAN,
+    armari_empotrat BOOLEAN,
+    garatge VARCHAR(48),
+    jardi BOOLEAN,
+    calefaccio VARCHAR(48),
+    piscina BOOLEAN,
+    traster BOOLEAN,
+    xemeneia BOOLEAN,
+    terassa BOOLEAN,
+    balco BOOLEAN
+);
+
+CREATE TABLE `caracteristiques_vivendes` (
+    id_vivenda INT,
+    id_caracteristica INT AUTO_INCREMENT,
+    PRIMARY KEY(id_vivenda, id_caracteristica),
+    FOREIGN KEY (id_vivenda) REFERENCES vivenda(id_vivenda),
+    FOREIGN KEY (id_caracteristica) REFERENCES caracteristiques(id_caracteristica)
+);
+
+CREATE TABLE `provincia` (
+    id_provincia INT AUTO_INCREMENT PRIMARY KEY,
+    nom_oficial VARCHAR(64) UNIQUE,
+    nom_catala VARCHAR(64) UNIQUE
+);
+
+CREATE TABLE `comarca` (
+    id_comarca INT AUTO_INCREMENT PRIMARY KEY,
+    nom_comarca VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE `municipi` (
+    id_municipi INT AUTO_INCREMENT,
+    id_provincia INT,
+    nom_municipi VARCHAR(100),
+    id_comarca INT,
+    codi_INE CHAR(5),
+    utmX VARCHAR(100),
+    utmY VARCHAR(100),
+    latitud DECIMAL(9,6),
+    longitud DECIMAL(9,6),
+    coord_vecvtorial VARCHAR(100),
+    PRIMARY KEY(id_municipi, id_provincia),
+    FOREIGN KEY (id_provincia) REFERENCES provincia(id_provincia),
+    FOREIGN KEY (id_comarca) REFERENCES comarca(id_comarca)
+);
+
+CREATE TABLE `consum_energetic` (
+    id_vivenda INT PRIMARY KEY,
+    consum ENUM('A', 'B', 'C', 'D', 'E', 'F', 'G'),
+    emisions ENUM('A', 'B', 'C', 'D', 'E', 'F', 'G'),
+    FOREIGN KEY (id_vivenda) REFERENCES vivenda(id_vivenda)
+);
+
+CREATE TABLE `municipi_vivenda` (
+    id_vivenda INT PRIMARY KEY,
+    id_municipi INT,
+    id_provincia INT,
+    FOREIGN KEY (id_vivenda) REFERENCES vivenda(id_vivenda),
+    FOREIGN KEY (id_municipi) REFERENCES municipi(id_municipi),
+    FOREIGN KEY (id_provincia) REFERENCES municipi(id_provincia)
+);
