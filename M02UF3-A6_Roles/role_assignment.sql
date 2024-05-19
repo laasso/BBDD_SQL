790cb7bf380064e034027d6d0eef23478a958777
laasso <nrubialesguerrero@gmail.com>
2024-05-19 14:53:09 +0200

    rol assigment

---
+++ M02UF3-A6_Roles/role_assignment.sql
@@ -0,0 +1,4 @@
+-- Assignar rols als usuaris
+GRANT venedor TO 'Venedor1'@'localhost', 'Venedor2'@'localhost', 'Venedor3'@'localhost';
+GRANT director TO 'Director1'@'localhost', 'Director2'@'localhost';
+GRANT gerent TO 'Gerent'@'localhost', 'Director1'@'localhost', 'Director2'@'localhost';
