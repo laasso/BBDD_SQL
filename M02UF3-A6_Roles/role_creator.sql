67dc98b228f24b19edd6da650256fb61138a43ad
laasso <nrubialesguerrero@gmail.com>
2024-05-19 14:50:43 +0200

    crear i assingar rols

---
+++ M02UF3-A6_Roles/role_creator.sql
@@ -0,0 +1,11 @@
+-- Crear rols i assignar privilegis
+CREATE ROLE Venedor;
+GRANT SELECT, INSERT, UPDATE ON vk22.comandes TO Venedor;
+GRANT SELECT, INSERT, UPDATE ON vk22.detall TO Venedor;
+GRANT SELECT ON vk22.clients TO Venedor;
+
+CREATE ROLE Direcotor;
+GRANT SELECT, UPDATE, DELETE, DROP, ALTER ON vk22.* TO Direcotor;
+
+CREATE ROLE Gerent;
+GRANT ALL PRIVILEGES ON vk22.* TO Gerent;
