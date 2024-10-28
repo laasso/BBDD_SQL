8c3c56bf90bee0db016895e9f2a0619df8974d79
laasso <nrubialesguerrero@gmail.com>
2024-10-28 16:06:32 +0100

    after insert conversio adn a arn

--- M10UF1-NF1A3/triggers.sql
+++ M10UF1-NF1A3/triggers.sql
@@ -80,3 +80,26 @@ END $$
 DELIMITER ;
 
 
+DELIMITER //
+CREATE TRIGGER convertir_adn_a_arn
+AFTER INSERT ON adn_table
+FOR EACH ROW
+BEGIN
+    DECLARE cadena_adn VARCHAR(255);
+    DECLARE cadena_arn VARCHAR(255);
+
+    -- Almacenar la cadena de ADN insertada
+    SET cadena_adn = NEW.cadena_adn;
+    
+    -- Reemplazar T por U en la cadena de ADN para convertir a ARN
+    SET cadena_arn = REPLACE(cadena_adn, 'T', 'U');
+    
+    -- Actualizar la columna ARN en la tabla con la cadena convertida
+    UPDATE adn_table
+    SET cadena_arn = cadena_arn
+    WHERE id = NEW.id;
+END;
+//
+DELIMITER ;
+
+
